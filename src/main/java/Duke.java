b91b9f1877325165d9839d8aaebe3a3a139dec64
KennethEer <59533849+KennethEer@users.noreply.github.com>
2020-08-26 16:52:52 +0800

    Add Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,8 +5,8 @@ public class Duke {
     private static final String GREETING = "Hello! I'm Duke";
     private static final String REQUEST = "What can I do for you?";
     private static final String EXIT = "Bye. Hope to see you again soon!";
-    private static int taskNumber = 0;
-    private static String[] tasks = new String[100];
+    private static int totalTaskNumber = 0;
+    private static Task[] tasks = new Task[100];
 
     public static void printGreeting() {
         System.out.println(LONG_LINE + System.lineSeparator() + GREETING);
@@ -14,8 +14,9 @@ public class Duke {
     }
 
     public static void printCommand(String input) {
-        tasks[taskNumber] = input;
-        taskNumber++;
+        Task t = new Task(input);
+        tasks[totalTaskNumber] = t;
+        totalTaskNumber++;
         System.out.println(LONG_LINE + System.lineSeparator() + "added: " + input + System.lineSeparator() + LONG_LINE);
     }
 
@@ -25,9 +26,27 @@ public class Duke {
 
     public static void printList() {
         System.out.println(LONG_LINE);
-        for (int i = 0; i < taskNumber; i++) {
-            System.out.println(i+1 + ". " + tasks[i]);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < totalTaskNumber; i++) {
+            System.out.println(i+1 + "." + tasks[i].getStatusIcon() + tasks[i].description);
         }
+        System.out.println(LONG_LINE);
+    }
+
+    public static void completeTask(String input) {
+        if (input.length() <= 5) {
+            return;
+        }
+        String taskDigit = input.substring(5);
+        int taskNumber = Integer.parseInt(taskDigit);
+        if (taskNumber > totalTaskNumber) {
+            return;
+        }
+        Task taskToFinish = tasks[taskNumber - 1];
+        taskToFinish.doTask();
+        System.out.println(LONG_LINE);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(taskToFinish.getStatusIcon() + taskToFinish.description);
         System.out.println(LONG_LINE);
     }
 
@@ -40,6 +59,8 @@ public class Duke {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 printList();
+            } else if (input.contains("done")) {
+                completeTask(input);
             } else {
                 printCommand(input);
             }
