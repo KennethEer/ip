8294e8aae837f3695fea30217f39ecee0846ee0a
KennethEer <59533849+KennethEer@users.noreply.github.com>
2020-08-26 12:20:01 +0800

    Add Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,31 @@
-public class Duke {
-    public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        */
+import java.util.Scanner;
 
-        String long_line = "____________________________________________________________";
-        String greeting = "Hello! I'm Duke";
-        String request = "What can I do for you?";
-        String exit = "Bye. Hope to see you again soon!";
-        
-        System.out.println(long_line);
-        System.out.println(greeting);
-        System.out.println(request);
-        System.out.println(long_line);
-        System.out.println(exit);
-        System.out.println(long_line);
+public class Duke {
+    private static final String LONG_LINE = "____________________________________________________________";
+    private static final String GREETING = "Hello! I'm Duke";
+    private static final String REQUEST = "What can I do for you?";
+    private static final String EXIT = "Bye. Hope to see you again soon!";
+
+    public static void printGreeting() {
+        System.out.println(LONG_LINE + System.lineSeparator() + GREETING);
+        System.out.println(REQUEST + System.lineSeparator() + LONG_LINE);
+    }
+
+    public static void printCommand(String input) {
+        System.out.println(LONG_LINE + System.lineSeparator() + input + System.lineSeparator() + LONG_LINE);
+    }
+
+    public static void main(String[] args) {
+        printGreeting();
+
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            printCommand(input);
+            input = in.nextLine();
+        }
+
+        printCommand(EXIT);
     }
 }
